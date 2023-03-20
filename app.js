a0775544cfc5b3ce9cc245fca7e82756f2664ece
Corentin Braud <corentin.braud95@gmail.com>
2023-03-20 11:35:19 +0100

    Modification tableau des forêts et simplification code pour remplacement texte

--- app.js
+++ app.js
@@ -6,36 +6,35 @@ boutonScroll.addEventListener("click", function () {
   element.scrollIntoView();
 });
 
-// Tableau avec les lieux
-const textesDescription = [
-  {
-    nameForest: "Forêt en Bretagne",
-    locationForest: "En Bretagne"
-  },
-  {
-    nameForest: "Forêt en Normandie",
-    locationForest: "En Normandie"
-  },
-  {
-    nameForest: "Forêt en Ile-de-France",
-    locationForest: "En Ile-de-France"
-  },
-  {
-    nameForest: "Forêt en Bretagne",
-    locationForest: "En Bretagne"
-  },
-  {
-    nameForest: "Forêt en Bretagne",
-    locationForest: "En Bretagne"
-  },
-  {
-    nameForest: "Forêt en Bretagne",
-    locationForest: "En Bretagne"
-  },
-  {
-    nameForest: "Forêt en Bretagne",
-    locationForest: "En Bretagne"
-  },
+// Tableau avec les forêts
+const forests = [
+  { nameForest: "Bois des Vents Éternels", locationForest: "Valbelle" },
+  { nameForest: "Forêt des Ombres Profondes", locationForest: "Noirebois" },
+  { nameForest: "Bois des Chants d'Automne", locationForest: "Doréclair" },
+  { nameForest: "Forêt des Mille Étoiles", locationForest: "Astridale" },
+  { nameForest: "Bois des Flammes Ardentes", locationForest: "Feulieu" },
+  { nameForest: "Forêt des Lumières Étincelantes", locationForest: "Brilleville" },
+  { nameForest: "Bois des Marais Endormis", locationForest: "Sylvebrume" },
+  { nameForest: "Forêt des Âmes en Peine", locationForest: "Mourneland" },
+  { nameForest: "Bois des Murmures Silencieux", locationForest: "Chuchoterre" },
+  { nameForest: "Forêt des Arbres Solitaires", locationForest: "Isolement" },
+  { nameForest: "Bois des Fleurs Enchantées", locationForest: "Féerie" },
+  { nameForest: "Forêt des Écailles d'Argent", locationForest: "Argentine" },
+  { nameForest: "Bois des Secrets Perdus", locationForest: "Cacheterre" },
+  { nameForest: "Forêt des Créatures Mystiques", locationForest: "Mystica" },
+  { nameForest: "Bois des Tempêtes Épiques", locationForest: "Tempêtevie" },
+  { nameForest: "Forêt des Regards de Braise", locationForest: "Braisefeu" },
+  { nameForest: "Bois des Sages Anciens", locationForest: "Sagessebois" },
+  { nameForest: "Forêt des Sources de Lumière", locationForest: "Lumièrebrume" },
+  { nameForest: "Bois des Éclats de Lune", locationForest: "Lunessence" },
+  { nameForest: "Forêt des Morts-Vivants", locationForest: "Nécrotis" },
+  { nameForest: "Bois des Feuilles de Cristal", locationForest: "Cristalbois" },
+  { nameForest: "Forêt des Dragons Légendaires", locationForest: "Dragontopie" },
+  { nameForest: "Bois des Échos Éternels", locationForest: "Échoforêt" },
+  { nameForest: "Forêt des Rêves Infinis", locationForest: "Rêverie" },
+  { nameForest: "Bois des Brumes Énigmatiques", locationForest: "Nébulie" },
+  { nameForest: "Forêt des Étoiles Scintillantes", locationForest: "Étoilelumière" },
+  { nameForest: "Bois des Racines Profondes", locationForest: "Racineroc" }
 ];
 
 // Récupération bouton en passant par le parent
@@ -55,11 +54,8 @@ for (let i = 0; i < picto.length; i++) {
     // Ajoute la class active au btn cliqué
     this.className += " active";
 
-    // Récupération des "h4" et "p"
-    document.getElementById("forest-name").innerHTML = name;
-    document.getElementById("forest-location").innerHTML = location;
-    // Remplacer le texte dans les éléments avec le texte approprié
-    const name = textesDescription[i].nameForest;
-    const location = textesDescription[i].locationForest;
+    // Récupération des "h4" et "p" pour les modifier avec les données du tableau
+    document.getElementById("forest-name").innerHTML = forests[i].nameForest;
+    document.getElementById("forest-location").innerHTML = forests[i].locationForest;
   });
 }
